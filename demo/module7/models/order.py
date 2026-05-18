e6c852ec9eeb891a23ca0b0ca64f13b1393f8786
Rekvizit <146362477+RKviZitNT@users.noreply.github.com>
2026-05-18 13:30:50 +0300

    Delete demo/module7 directory

--- demo/module7/models/order.py
+++
@@ -1,7 +0,0 @@
-class Order:
-    def __init__(self, client, city, phone, date, amount):
-        self.client = client
-        self.city = city
-        self.phone = phone
-        self.date = date
-        self.amount = float(amount)
